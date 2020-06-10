a4cd60e142df7d3bb6f99d400bdcee4e82d98cd7
Flamarion Jorge <flamarion@hashicorp.com>
2020-06-10 10:58:27 +0200

    Fixing the output

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 data "archive_file" "lambda_zip" {
   type = "zip"
   source_file = "${path.module}/lambda/xpto.py"
-  output_path = "${path.module}/lambda/xpto.py"
+  output_path = "${path.module}/lambda/xpto.zip"
 }
 
 resource "null_resource" "test" {
