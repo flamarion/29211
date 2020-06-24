a2e77ad5aafee64197d78fa022a004e501a87ba6
Flamarion Jorge <flamarion@hashicorp.com>
2020-06-24 10:28:52 +0200

    Testing

--- main.tf
+++ main.tf
@@ -1,5 +1,33 @@
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "FlamarionLab"
+
+    workspaces {
+      name = "tf-demo-2"
+    }
+  }
+}
+
+data "tfe_team" "C3PO" {
+  name         = "Owners"
+  organization = "FlamarionLab"
+}
+
+data "tfe_ssh_key" "team_key" {
+  name         = "ssh-key-demo"
+  organization = "FlamarionLab"
+}
+
+output "tfe_things" {
+  value = [
+    data.tfe_team.C3PO,
+    data.tfe_ssh_key.team_key
+  ]
+}
+
 data "archive_file" "lambda_zip" {
-  type = "zip"
+  type        = "zip"
   source_file = "${path.module}/lambda/xpto.py"
   output_path = "${path.module}/lambda/xpto.zip"
 }
