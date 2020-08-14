d4bf138210262b00681ddb4858beee35d3914164
Flamarion Jorge <flamarion@hashicorp.com>
2020-08-14 11:21:36 +0200

    Testing remote state

--- main.tf
+++ main.tf
@@ -4,6 +4,18 @@ data "archive_file" "lambda_zip" {
   output_path = "${path.module}/lambda/xpto.zip"
 }
 
+data "terraform_remote_state" "flamarion_lab" {
+  backend = "remote"
+  #token = "PG7VuF5ybzObkQ.atlasv1.xaFseHG53Sv2vw8m9jrazg5NlQv5jsgkjgV8v7iQQDla3Oqh87AbsZHUsMt6zgEJwWE"
+  config = {
+    organization = "FlamarionLab"
+
+    workspaces = {
+      name = "31633"
+    }
+  }
+}
+
 resource "null_resource" "test" {
   triggers = {
     always_run = timestamp()
@@ -19,4 +31,8 @@ output "hash" {
 
 output "path" {
   value = data.archive_file.lambda_zip.output_path
+}
+
+output "flamarion_lab_org_output" {
+  value = data.terraform_remote_state.flamarion_lab.master
 }
